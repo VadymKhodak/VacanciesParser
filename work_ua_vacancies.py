9c31c0c8664645fee447a42e89161260ef0e1657
vadymkhodak <vaddimart@gmail.com>
2019-02-19 19:46:54 +0200

    Update  work_ua_vacancies.py and ll.py

--- work_ua_vacancies.py
+++ work_ua_vacancies.py
@@ -187,4 +187,4 @@ def get_vacancies(cities=['kyiv'], categories=['it']):
     return pd.read_sql(f'SELECT * FROM public."work_ua_vacancies1_{datetime.date.today()}"', engine)
 
 
-print(get_vacancies())
+print(get_cities())
